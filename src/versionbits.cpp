7e65e588c53a281f9ef8ac475183d2bea2c44437
sidhujag <sidhujag@hotmail.com>
2016-09-23 18:34:47 -0700

    base version

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -122,7 +122,8 @@ protected:
 
     bool Condition(const CBlockIndex* pindex, const Consensus::Params& params) const
     {
-        return (((pindex->nVersion & VERSIONBITS_TOP_MASK) == VERSIONBITS_TOP_BITS) && (pindex->nVersion & Mask(params)) != 0);
+		// SYSCOIN
+        return (((pindex->nVersion.GetBaseVersion() & VERSIONBITS_TOP_MASK) == VERSIONBITS_TOP_BITS) && (pindex->nVersion.GetBaseVersion() & Mask(params)) != 0);
     }
 
 public:
