23bfe0752f4e037313d84ba90dd305be2341e0cf
sidhujag <sidhujag@hotmail.com>
2016-09-27 13:42:00 -0700

    typo

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -376,7 +376,7 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
             if (!(CheckTransaction(wtx, state) && (wtx.GetHash() == hash) && state.IsValid()))
 			{
 				// SYSCOIN
-				if(wtx.nVersion == GetSyscoinTxVersion())
+				if(wtx.GetHash() != hash && wtx.nVersion == GetSyscoinTxVersion())
 					return true;
 				strErr = "Error reading wallet database. CheckTransaction failed, validation state: " + FormatStateMessage(state);
                 return false;
