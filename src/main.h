50692973a12846e26584fdef1934302ab1c4f009
sidhujag <sidhujag@hotmail.com>
2016-09-27 00:13:11 -0700

    checklevel 1

--- src/main.h
+++ src/main.h
@@ -195,7 +195,7 @@ static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
 
 static const signed int DEFAULT_CHECKBLOCKS = 6;
 // SYSCOIN
-static const unsigned int DEFAULT_CHECKLEVEL = 2;
+static const unsigned int DEFAULT_CHECKLEVEL = 1;
 
 // Require that user allocate at least 550MB for block & undo files (blk???.dat and rev???.dat)
 // At 1MB per block, 288 blocks = 288MB.
