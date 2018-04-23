abcb9dcc6d039ca0968aaefce540e8894930f879
sidhujag <sidhujag@hotmail.com>
2018-04-22 18:15:47 -0700

    conmpile

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -309,6 +309,7 @@ void CMasternode::UpdateLastPaid(const CBlockIndex *pindex, int nMaxBlocksToScan
 
     LOCK(cs_mapMasternodeBlocks);
 	CMasternodePayee payee;
+	CAmount nTotal;
     for (int i = 0; BlockReading && BlockReading->nHeight > nBlockLastPaid && i < nMaxBlocksToScanBack; i++) {
         if(mnpayments.mapMasternodeBlocks.count(BlockReading->nHeight) &&
             mnpayments.mapMasternodeBlocks[BlockReading->nHeight].HasPayeeWithVotes(mnpayee, 2, payee))
