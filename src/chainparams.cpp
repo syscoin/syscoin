129077905f4db7a476a5c6a72e6c440b9125cf27
sidhujag <sidhujag@hotmail.com>
2018-04-22 22:43:02 -0700

    compile

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -137,23 +137,6 @@ static void GenerateGenesisBlock(CBlockHeader &genesisBlock, uint256 *phash)
 	printf("Generate hash = %s\n", (*phash).ToString().c_str());
 	printf("genesis.hashMerkleRoot = %s\n", genesisBlock.hashMerkleRoot.ToString().c_str());
 }
-/**
-* Build the genesis block. Note that the output of its generation
-* transaction cannot be spent since it did not originally exist in the
-* database.
-*
-* CBlock(hash=000000000019d6, ver=1, hashPrevBlock=00000000000000, hashMerkleRoot=4a5e1e, nTime=1231006505, nBits=1d00ffff, nNonce=2083236893, vtx=1)
-*   CTransaction(hash=4a5e1e, ver=1, vin.size=1, vout.size=1, nLockTime=0)
-*     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
-*     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
-*   vMerkleTree: 4a5e1e
-*/
-static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
-{
-	const char* pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
-	const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
-	return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
-}
 
 static CBlock FindDevNetGenesisBlock(const Consensus::Params& params, const CBlock &prevBlock, const CAmount& reward)
 {
