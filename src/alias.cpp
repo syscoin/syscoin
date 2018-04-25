1db8f44cd0ce12d0396c29906a5b034afe91aa28
sidhujag <sidhujag@hotmail.com>
2018-04-25 11:01:55 -0700

    compile

--- src/alias.cpp
+++ src/alias.cpp
@@ -1010,8 +1010,7 @@ void CreateRecipient(const CScript& scriptPubKey, CRecipient& recipient)
 	recipient = recp;
 	CTxOut txout(recipient.nAmount, scriptPubKey);
 	size_t nSize = GetSerializeSize(txout, SER_DISK, 0) + 148u;
-	nFee = 3 * minRelayTxFee.GetFee(nSize);
-	recipient.nAmount = nFee;
+	recipient.nAmount = 3 * minRelayTxFee.GetFee(nSize);
 }
 void CreateFeeRecipient(CScript& scriptPubKey, const vector<unsigned char>& data, CRecipient& recipient)
 {
