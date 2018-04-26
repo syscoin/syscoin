2ae3095a9f2cd967be0e998f674cab8c9c1bbbbe
sidhujag <sidhujag@hotmail.com>
2018-04-26 11:35:01 -0700

    compile

--- src/test/syscoin_alias_tests.cpp
+++ src/test/syscoin_alias_tests.cpp
@@ -578,7 +578,7 @@ BOOST_AUTO_TEST_CASE (generate_aliasbalancewithtransfer)
 	GenerateBlocks(5, "node2");
 	BOOST_CHECK_NO_THROW(r = CallRPC("node3", "aliasbalance jagnodebalance2"));
 	balanceAfter = AmountFromValue(find_value(r.get_obj(), "balance"));
-	BOOST_CHECK_EQUAL(balanceAfter, 12.1*COIN-10*COIN)+balanceAfterTransfer);
+	BOOST_CHECK_EQUAL(balanceAfter, 12.1*COIN-10*COIN+balanceAfterTransfer);
 
 	// edit and balance should remain the same
 	hex_str = AliasUpdate("node3", "jagnodebalance2", "pubdata1");
