080cfbc7d4a76501282c26f5a1568af1ed5f4580
jagdeep sidhu <sidhujag@syscoin.org>
2025-04-16 17:10:06 -0700

    Update bswap_tests.cpp

--- src/test/bswap_tests.cpp
+++ src/test/bswap_tests.cpp
@@ -17,8 +17,8 @@ BOOST_AUTO_TEST_CASE(bswap_tests)
     uint32_t e2 = 0xbc9a7856;
     uint64_t e3 = 0xbc9a78563412f0de;
     BOOST_CHECK(internal_bswap_16(u1) == e1);
-    BOOST_CHECK(internal_bswap_16(u2) == e2);
-    BOOST_CHECK(internal_bswap_16(u3) == e3);
+    BOOST_CHECK(internal_bswap_32(u2) == e2);
+    BOOST_CHECK(internal_bswap_64(u3) == e3);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
